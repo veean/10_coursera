151b9e9b527d28560c1d660dc52d632c770d7534
veean <nersesowm@gmail.com>
2017-01-09 01:36:11 +0400

    add get courses func

--- coursera.py
+++ coursera.py
@@ -1,7 +1,15 @@
+import bs4
+from lxml import etree
+import openpyxl
+import requests
 
+
+COURSES_URL = "https://www.coursera.org/sitemap~www~courses.xml"
 
 def get_courses_list():
-    pass
+    response = requests.get(COURSES_URL)
+    root = etree.fromstring(response.content)
+    return [url.text for url in root.iter("{*}loc")]
 
 
 def get_course_info(course_slug):

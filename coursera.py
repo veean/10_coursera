1203fec183251d2f85424908c3e61a1a4327fc27
veean <nersesowm@gmail.com>
2017-01-11 01:18:17 +0400

    fix output to xlsx and fill main function

--- coursera.py
+++ coursera.py
@@ -1,11 +1,19 @@
 from bs4 import BeautifulSoup
-from lxml import etree
 from openpyxl import Workbook
+from lxml import etree
 import requests
+import argparse
 import random
 import json
 
+
 COURSES_URL = "https://www.coursera.org/sitemap~www~courses.xml"
+
+
+def get_path_argument():
+    parser = argparse.ArgumentParser(description='Output file for information about <Coursera> courses')
+    parser.add_argument('file', type=str, help='specify .xlsx file to output')
+    return parser.parse_args().file
 
 
 def get_courses_list(sample_size=20):
@@ -18,8 +26,10 @@ def fetch_course_start_date(beautiful_soup_object):
     start_date_footprint = 'startDate'
     json_with_possible_start_date = beautiful_soup_object.find('script', {'type': 'application/ld+json'})
     if json_with_possible_start_date and start_date_footprint in json_with_possible_start_date.text:
-        datetime = json.loads(json_with_possible_start_date.text)['hasCourseInstance'][0]['startDate']
+        datetime = json.loads(json_with_possible_start_date.text)['hasCourseInstance'][0][start_date_footprint]
         return datetime
+    else:
+        return 'No info about start date'
 
 
 def fetch_course_name(beautiful_soup_object):
@@ -28,45 +38,51 @@ def fetch_course_name(beautiful_soup_object):
         return course_name.text
 
 
+def fetch_course_rate(beautiful_soup_object):
+    course_rate = beautiful_soup_object.find('div', {'class': 'ratings-text bt3-visible-xs'})
+    return course_rate.text if course_rate else 'Unrated'
+
+
 def fetch_course_language(beautiful_soup_object):
     course_language = beautiful_soup_object.find('div', {'class': 'language-info'})
     if course_language:
         return course_language.text
 
 
-def fetch_course_rate(beautiful_soup_object):
-    course_rate = beautiful_soup_object.find('div', {'class': 'ratings-text bt3-visible-xs'})
-    if course_rate:
-        return course_rate.text
-
-
 def get_course_info(course_slug):
+    courses_list = []
     for course in course_slug:
         page = requests.get(course).content
         soup_object = BeautifulSoup(page, 'html.parser')
+        duration = soup_object.find_all('div', {'class': 'week'})
+
         course_name = fetch_course_name(soup_object)
-        course_language = fetch_course_language(soup_object)
-        course_start_date = fetch_course_start_date(soup_object)
-        course_duration = len(soup_object.find_all('div', {'class': 'week'}))
         course_rate = fetch_course_rate(soup_object)
-        return course_name, course, course_language, course_start_date, course_duration, course_rate
+        course_language = fetch_course_language(soup_object)
+        course_duration = len(duration) if duration else 'Unknown duration'
+        course_start_date = fetch_course_start_date(soup_object)
+
+        courses_list.append((course_name, course, course_language, course_start_date, course_duration, course_rate))
+    return courses_list
 
 
-def output_courses_info_to_xlsx(courses_tuple, filepath='C:\\Users\\Vadim\\Desktop\\123.xlsx'):
+def output_courses_info_to_xlsx(courses_list, filepath):
     courses_workbook = Workbook()
     workbook_page = courses_workbook.active
     column_names = ['Course name', 'URL', 'Language', 'Start date', 'Duration', 'Average rating']
 
-    for index, name in enumerate(column_names,1):
+    for index, name in enumerate(column_names, 1):
         workbook_page.cell(row=1, column=index).value = name
 
-    for position, course_info in enumerate(courses_tuple, 2):
+    for position, course_info in enumerate(courses_list, 2):
         for fetched_parameter_id, fetched_parameter in enumerate(course_info, 1):
             workbook_page.cell(row=position, column=fetched_parameter_id).value = fetched_parameter
 
     courses_workbook.save(filepath)
+    return True
 
 
 if __name__ == '__main__':
-    # print(get_course_info(get_courses_list()))
-    output_courses_info_to_xlsx(get_course_info(get_courses_list()))
+    print('Collecting information about courses... wait a minute please...')
+    if output_courses_info_to_xlsx(get_course_info(get_courses_list()), get_path_argument()):
+        print('File saved to {}'.format(get_path_argument()))

6d754b3b62880e6c9b3bafc5e766a085671eba7c
veean <nersesowm@gmail.com>
2017-01-16 02:25:37 +0400

    decompose some functions formatting None output in one place

--- coursera.py
+++ coursera.py
@@ -16,8 +16,12 @@ def get_path_argument():
     return parser.parse_args().file
 
 
+def fetch_url_content(url):
+    return requests.get(url).content
+
+
 def get_courses_list(sample_size=20):
-    courses_raw_info = requests.get(COURSES_URL).content
+    courses_raw_info = fetch_url_content(COURSES_URL)
     root = etree.fromstring(courses_raw_info)
     return [course_unit[0].text for course_unit in random.sample(list(root), sample_size)]
 
@@ -28,8 +32,6 @@ def fetch_course_start_date(beautiful_soup_object):
     if json_with_possible_start_date and start_date_footprint in json_with_possible_start_date.text:
         datetime = json.loads(json_with_possible_start_date.text)['hasCourseInstance'][0][start_date_footprint]
         return datetime
-    else:
-        return 'No info about start date'
 
 
 def fetch_course_name(beautiful_soup_object):
@@ -40,7 +42,7 @@ def fetch_course_name(beautiful_soup_object):
 
 def fetch_course_rate(beautiful_soup_object):
     course_rate = beautiful_soup_object.find('div', {'class': 'ratings-text bt3-visible-xs'})
-    return course_rate.text if course_rate else 'Unrated'
+    return course_rate.text if course_rate else None
 
 
 def fetch_course_language(beautiful_soup_object):
@@ -49,17 +51,17 @@ def fetch_course_language(beautiful_soup_object):
         return course_language.text
 
 
-def get_course_info(course_slug):
+def get_course_info(courses_links):
     courses_list = []
-    for course in course_slug:
-        page = requests.get(course).content
+    for course in courses_links:
+        page = fetch_url_content(course)
         soup_object = BeautifulSoup(page, 'html.parser')
         duration = soup_object.find_all('div', {'class': 'week'})
 
         course_name = fetch_course_name(soup_object)
         course_rate = fetch_course_rate(soup_object)
         course_language = fetch_course_language(soup_object)
-        course_duration = len(duration) if duration else 'Unknown duration'
+        course_duration = len(duration) if duration else None
         course_start_date = fetch_course_start_date(soup_object)
 
         courses_list.append((course_name, course, course_language, course_start_date, course_duration, course_rate))
@@ -76,7 +78,8 @@ def output_courses_info_to_xlsx(courses_list, filepath):
 
     for position, course_info in enumerate(courses_list, 2):
         for fetched_parameter_id, fetched_parameter in enumerate(course_info, 1):
-            workbook_page.cell(row=position, column=fetched_parameter_id).value = fetched_parameter
+            workbook_page.cell(row=position, column=fetched_parameter_id).value = fetched_parameter if \
+                fetched_parameter else 'No info'
 
     courses_workbook.save(filepath)
     return True
